70dc251f0f457a89da7947200a68b29a78b55165
Majad In <dev3thoughts@gmail.com>
2020-12-16 22:11:10 +0100

    added lite-server :tada:

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
-
-function add( num1: number, num2: number) {
-  return parseInt(num1 + num2);
+function add(n1: number, n2: number, show: boolean) {
+  if (show) {
+    console.log("Pass");
+  }
+  return n1 + n2;
 }
 
-button.addEventListener("click", function () {
-  console.log(add(input1.value, input2.value));
-});
+const res = true;
+const result = add(45, 4.6, res);
+
+console.log(result);
